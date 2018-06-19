9076ab6584f5ae85f2c9f93b9b6cd7fea323277b
Berk Erol <berk.erol@boun.edu.tr>
2018-06-19 03:33:15 +0200

    better animation and explosion

--- fireworks.js
+++ fireworks.js
@@ -13,15 +13,26 @@ let mouse = {
 let firework = {
   color: 15,
   colors: [[240, 60, 80], [255, 100, 0], [255, 160, 0], [255, 220, 0], [220, 255, 0], [160, 255, 0], [50, 255, 0], [0, 220, 120], [130, 230, 220], [0, 220, 240], [240, 120, 255], [210, 140, 170], [220, 180, 240], [160, 220, 220], [200, 200, 200]],
-  lineCap: 'round',
   lines: [20, 24, 30, 36, 40],
-  highestAlpha: 0.015,
+  lineCap: 'round',
+  explosionAlpha: 0.4,
+  highestAlphaDecrease1: 0.015,
+  highestAlphaDecrease2: 0.02,
   highestLineWidth: 2.5,
-  highestSpeed: 1.0,
-  lowestAlpha: 0.005,
+  highestRadius: 2.5,
+  highestSpeed: 2.0,
+  highestSpeedDecrease: 0.02,
+  highestStep: 0.3,
+  highestVariance: 0.04,
+  lowestAlphaDecrease1: 0.005,
+  lowestAlphaDecrease2: 0.01,
   lowestLineWidth: 1.5,
-  lowestSpeed: 0.5,
-  probability: 0.2
+  lowestRadius: 1.5,
+  lowestSpeed: 1.0,
+  lowestSpeedDecrease: 0.01,
+  lowestStep: 0.1,
+  lowestVariance: -0.04,
+  probability: 0.1
 };
 
 let fireworks = [];
@@ -43,17 +54,34 @@ function draw () {
 }
 
 function drawFirework (f) {
-  ctx.lineWidth = f.lineWidth;
-  ctx.strokeStyle = 'rgba(' + f.color[0] + ',' + f.color[1] + ',' + f.color[2] + ',' + f.alpha + ')';
-  for (let i = 0; i < f.lines / 2; i++) {
-    let angle = i * 2 * Math.PI / f.lines;
-    let x = f.length * Math.cos(angle);
-    let y = f.length * Math.sin(angle);
-    ctx.beginPath();
-    ctx.moveTo(f.x - x, f.y - y);
-    ctx.lineTo(f.x + x, f.y + y);
-    ctx.stroke();
-    ctx.closePath();
+  if (f.alpha1 > 0) {
+    ctx.lineWidth = f.lineWidth;
+    ctx.strokeStyle = 'rgba(' + f.color[0] + ',' + f.color[1] + ',' + f.color[2] + ',' + f.alpha1 + ')';
+    for (let i = 0; i < f.lines / 2; i++) {
+      let angle = i * 2 * Math.PI / f.lines;
+      let x = f.length * Math.cos(angle);
+      let y = f.length * Math.sin(angle);
+      ctx.beginPath();
+      ctx.moveTo(f.x - x - x * f.variances[i], f.y - y - y * f.variances[i]);
+      ctx.lineTo(f.x + x + x * f.variances[i], f.y + y + y * f.variances[i]);
+      ctx.stroke();
+      ctx.closePath();
+    }
+  }
+  if (f.alpha2 > 0) {
+    ctx.fillStyle = 'rgba(' + f.color[0] + ',' + f.color[1] + ',' + f.color[2] + ',' + f.alpha2 + ')';
+    for (let i = 1.0; i > f.step; i -= f.step) {
+      for (let j = 0; j < f.lines / 2; j++) {
+        let angle = j * 2 * Math.PI / f.lines;
+        let x = f.length * Math.cos(angle) * i;
+        let y = f.length * Math.sin(angle) * i;
+        ctx.beginPath();
+        ctx.arc(f.x - x - x * f.variances[j], f.y - y - y * f.variances[j], f.radius, 0, 2 * Math.PI);
+        ctx.arc(f.x + x + x * f.variances[j], f.y + y + y * f.variances[j], f.radius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
   }
 }
 
@@ -66,11 +94,22 @@ function createFireworks () {
 function removeFireworks () {
   for (let i = fireworks.length - 1; i >= 0; i--) {
     let f = fireworks[i];
-    if (f.alpha < 0) {
-      fireworks.splice(i, 1);
-    } else {
+    f.alpha1 -= f.alphaDecrease1;
+    if (f.alpha1 > firework.explosionAlpha) {
       f.length += f.speed;
-      f.alpha -= f.dec;
+      if (f.speed > firework.highestSpeedDecrease) {
+        f.speed -= f.speedDecrease;
+      } else {
+        f.speed = 0;
+      }
+    } else {
+      if (f.alpha2 === -1) {
+        f.alpha2 = 1;
+      } else if (f.alpha2 < 0) {
+        fireworks.splice(i, 1);
+      } else {
+        f.alpha2 -= f.alphaDecrease2;
+      }
     }
   }
 }
@@ -82,16 +121,27 @@ function createFirework (x, y) {
   } else {
     color = firework.colors[firework.color];
   }
+  let lines = firework.lines[Math.floor(Math.random() * firework.lines.length)];
+  let variances = [];
+  for (let i = 0; i < lines / 2; i++) {
+    variances.push(firework.lowestVariance + Math.random() * (firework.highestVariance - firework.lowestVariance));
+  }
   fireworks.push({
     x,
     y,
     color,
-    alpha: 1,
-    dec: firework.lowestAlpha + Math.random() * (firework.highestAlpha - firework.lowestAlpha),
+    alpha1: 1,
+    alpha2: -1,
+    alphaDecrease1: firework.lowestAlphaDecrease1 + Math.random() * (firework.highestAlphaDecrease1 - firework.lowestAlphaDecrease1),
+    alphaDecrease2: firework.lowestAlphaDecrease2 + Math.random() * (firework.highestAlphaDecrease2 - firework.lowestAlphaDecrease2),
     length: 0,
-    lines: firework.lines[Math.floor(Math.random() * firework.lines.length)],
+    lines,
     lineWidth: firework.lowestLineWidth + Math.random() * (firework.highestLineWidth - firework.lowestLineWidth),
-    speed: firework.lowestSpeed + Math.random() * (firework.highestSpeed - firework.lowestSpeed)
+    radius: firework.lowestRadius + Math.random() * (firework.highestRadius - firework.lowestRadius),
+    speed: firework.lowestSpeed + Math.random() * (firework.highestSpeed - firework.lowestSpeed),
+    speedDecrease: firework.lowestSpeedDecrease + Math.random() * (firework.highestSpeedDecrease - firework.lowestSpeedDecrease),
+    step: firework.lowestStep + Math.random() * (firework.highestStep - firework.lowestStep),
+    variances
   });
 }
 
